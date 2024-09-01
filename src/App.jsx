6fda098b8de195d52d83fb7af4dc25140ee93b99
saurabh sahu <saurabhhsahu9929@gmail.com>
2024-09-01 12:32:05 +0530

    okk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <h1 className=''>App</h1>
+    <div>
+      <h1 className=''>App</h1>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+    </div>
   )
 }
 
