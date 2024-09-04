d1f222f70c0a643262342072c63f1239e38ce74d
saurabhhsahu <saurabhhsahu9929@gmail.com>
2024-09-04 10:51:39 +0530

    added functionality in signup page

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './src/router/userRouter.js'; // Ensure the path is correct
 import authRouter from './src/router/authRouter.js';
 
 const app = express();
+app.use(cors())
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // Use userRouter for routes starting with /user
 app.use('/user',userRouter);
-app.use('/auth',authRouter);
+app.use('/api/auth',authRouter);
 
 export default app;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link,useNavigate} from 'react-router-dom'
 
 function Signout() {
+  const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+    })
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()    
+    setLoading(true)
+    const res = await fetch("http://localhost:3000/api/auth/signup",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(formData)
+      }
+    );
+    const data = await res.json();
+    setLoading(false);
+    
+    if(data.success === false)
+      setError(data.message);
+    setError(null);
+    navigate('/signin')
+    console.log(data);
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit = {handleSubmit} className='flex flex-col gap-4'>
         <input
          type="text" 
          placeholder='username'
          className='border-[1px] border-black p-3 rounded-lg'
          id='username'
+         onChange={handleChange}
         />
 
         <input
@@ -18,6 +53,7 @@ function Signout() {
          placeholder='email'
          className='border-[1px] border-black p-3 rounded-lg'
          id='email'
+         onChange={handleChange}
         />
 
         <input
@@ -25,9 +61,12 @@ function Signout() {
          placeholder='password'
          className='border-[1px] border-black p-3 rounded-lg'
          id='password'
+         onChange={handleChange}
         />
 
-        <button disabled className='bg-slate-700 text-white p-3 cursor-pointer rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 cursor-pointer rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'loading...' : 'signup'}
+        </button>
       </form>
 
       <div className='flex gap-4 my-4 justify-center'>
@@ -38,6 +77,7 @@ function Signout() {
             Sign in
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5 text-center'>{error}</p>}
     </div>
   )
 }
